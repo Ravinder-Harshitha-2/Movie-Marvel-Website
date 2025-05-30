9e3c7307ae4fa746266eb93ee1c968d0b54a3855
Ravinder-Harshitha <harshitharavinder2016@gmail.com>
2025-05-30 13:25:32 +0400

    dashboard created

---
+++ movie-marvel-website/dashboard.js
@@ -0,0 +1,24 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-app.js";
+import { getAuth, signOut } from "https://www.gstatic.com/firebasejs/11.8.1/firebase-auth.js";
+
+// Initialize Firebase
+const firebaseConfig = {
+    apiKey: "AIzaSyCKAGAZLcFNTsfZ23yUIGKv7T1y6jaMWKA",
+    authDomain: "moviewebsite-2ce81.firebaseapp.com",
+    projectId: "moviewebsite-2ce81",
+    storageBucket: "moviewebsite-2ce81.firebasestorage.app",
+    messagingSenderId: "978355164474",
+    appId: "1:978355164474:web:a038a7444020746ba76c03"
+};
+        
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
+
+document.getElementById("logoutBtn").addEventListener("click", () => {
+    signOut(auth).then(() => {
+        alert("Logged out!");
+        window.location.href = "first.html";
+    }).catch((error) => {
+        console.error("Logout error:", error);
+    });
+});
